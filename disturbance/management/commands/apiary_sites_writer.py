55c0506bb7f683c44995b1bdc8851e84b656a4b7
katsufumi shibata <katsufumi.shibata@dbca.wa.gov.au>
2025-04-01 11:32:50 +0800

    To consolidate all processes into a single script file for now, copy the relevant_applicant_phone_number function added to the model file

--- disturbance/management/commands/apiary_sites_writer.py
+++ disturbance/management/commands/apiary_sites_writer.py
@@ -6,6 +6,8 @@ import xlsxwriter
 from io import BytesIO
 from datetime import datetime
 from django.core.mail import EmailMessage
+from ledger.accounts.models import EmailUser
+from disturbance.components.organisations.models import Organisation
 
 
 class Command(BaseCommand):
@@ -14,6 +16,19 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('--org_name', type=str, help='Organisation Name', required=False)
+
+    def relevant_applicant_phone_number(self, relevant_applicant):
+        if isinstance(relevant_applicant, EmailUser):
+            return relevant_applicant.phone_number
+        elif isinstance(relevant_applicant, Organisation):
+            admins = relevant_applicant.contacts.filter(user_status__in=('active', 'suspended', 'contact_form',), is_admin=True)
+            admin = admins.first() if admins else None
+            if admin:
+                return admin.phone_number
+            else:
+                return ''
+        else:
+            return ''
         
     def handle(self, *args, **options):
 
@@ -42,9 +57,11 @@ class Command(BaseCommand):
                 'Address',
             ]
             sheet.write_row(0, col, line)
-            for row, asoa in enumerate(qs.order_by('approval__current_proposal__applicant'), 1):
+            excel_row = 1  # Start a separate counter for Excel rows
+            for asoa in qs.order_by('approval__current_proposal__applicant'):
                 region_district = get_region_district(asoa.wkb_geometry)
-                if 'FRANKLAND' in region_district:
+                if 'kalgoorlie' in region_district.lower():
+                    relevant_applicant_phone_number = self.relevant_applicant_phone_number(asoa.approval.relevant_applicant)
                     line = [
                         f'{asoa.approval.relevant_applicant_name}',
                         f'{asoa.apiary_site_id}',
@@ -53,18 +70,19 @@ class Command(BaseCommand):
                         f'{asoa.site_category.name}',
                         f'{asoa.licensed_site}',
                         f'{region_district}',
-                        f'{asoa.approval.relevant_applicant_phone_number}',
+                        f'{relevant_applicant_phone_number}',
                         f'{asoa.approval.relevant_applicant_email}',
                         f'{asoa.approval.relevant_applicant_address}',
                     ]
-                    sheet.write_row(row, col, line)
+                    sheet.write_row(excel_row, col, line)
+                    excel_row += 1
 
         email = EmailMessage(
             subject=f'Apiary Sites List - {date_str}',
             body='Please find attached Excel WB with list of Apiary Sites.',
             from_email='no-reply@dbca.wa.gov.au',
             #to=['apiary@dpaw.wa.gov.au'],
-            to=['jawaid.mushtaq@dbca.wa.gov.au'],
+            to=['katsufumi.shibata@dbca.wa.gov.au'],
             cc=[],
             reply_to=['no-reply@dbca.wa.gov.au'],
             headers=None,
